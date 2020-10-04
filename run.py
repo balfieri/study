b4a13f336d7980d751830a422b17ee96df636902
Robert A Alfieri <balfieri@nvidia.com>
2020-10-03 21:10:03 -0400

    renames

--- run.py
+++ run.py
@@ -67,25 +67,25 @@ while True:
     # choose questions
     #-----------------------------------------------------------------------
     count = question_cnt;
-    which = []
+    curr_questions = []
     asked = {}
     for i in range( count ):
         while True:
             ii = rand_n( max_question_cnt )
             if ii not in asked: break
         asked[ii] = True
-        which.append( ii )
+        curr_questions.append( ii )
 
     #-----------------------------------------------------------------------
     # keep trying to test questions that haven't been answered correctly
     #-----------------------------------------------------------------------
-    while len( which ) != 0:
+    while len( curr_questions ) != 0:
         for i in range( 100 ): print()
         correct_cnt = 0
-        count = len( which )
-        missed = []
-        while len( which ) != 0:
-            ii = which.pop( 0 )
+        count = len( curr_questions )
+        missed_questions = []
+        while len( curr_questions ) != 0:
+            ii = curr_questions.pop( 0 )
             q = questions[ii*2]
             a = questions[ii*2+1]
             a_lc = a.lower()
@@ -95,11 +95,11 @@ while True:
                 correct_cnt += 1
             else:
                 print( 'Wrong!  ' + a )
-                missed.append( ii )
+                missed_questions.append( ii )
         pct = int( 100.0 * correct_cnt / count + 0.5 )
         print( '\nYou got ' + str(correct_cnt) + ' out of ' + str(count) + ' questions correct (' + str(pct) + '%)' ) 
-        if len( missed ) != 0 and prompt( '\nRetry missed questions?', 'y' ) == 'y': 
-            which = missed
+        if len( missed_questions ) != 0 and prompt( '\nRetry missed questions?', 'y' ) == 'y': 
+            curr_questions = missed_questions
     if prompt( '\nPlay again?', 'y' ) != 'y': break 
 
 print( '\nGoodbye!\n' )
