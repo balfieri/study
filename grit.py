3f8bec1999bc131aa5b258bbc5d54a6794b39c0f
Robert A Alfieri <balfieri@nvidia.com>
2023-02-04 10:01:40 -0500

    options

--- grit.py
+++ grit.py
@@ -18,15 +18,32 @@ def match( s, pattern ):
 #-----------------------------------------------------------------------
 # process command line args
 #-----------------------------------------------------------------------
-subjects_s = 'italian_basic,italian_advanced,italian_expressions_common,italian_expressions_other,american_expressions_get,american_expressions_favorite,italian_vulgar,italian_passato_remoto'
-
-if len( sys.argv ) != 2: die( 'usage: grit.py <string>', '' )
+if len( sys.argv ) < 2: die( 'usage: grit.py <string> [options]', '' )
 string = sys.argv[1]
-subjects = subjects_s.split( ',' )
+subjects_s = 'italian_basic,italian_advanced,italian_expressions_common,italian_expressions_other,american_expressions_get,american_expressions_favorite,italian_vulgar,italian_passato_remoto'
+search_question = True
+search_answer = True
+out_file = ''
+i = 2
+while i < len( sys.argv ):
+    arg = sys.argv[i]
+    i += 1
+    if   arg == '-subjects':
+        subjects_s = sys.argv[i]
+        i += 1
+    elif arg == '-q':
+        search_question = int(sys.argv[i])
+        i += 1
+    elif arg == '-a':
+        search_answer = int(sys.argv[i])
+        i += 1
+    else:
+        die( f'unknown option: {arg}' )
 
 #-----------------------------------------------------------------------
 # read in <subject>.txt files
 #-----------------------------------------------------------------------
+subjects = subjects_s.split( ',' )
 for subject in subjects:
     filename = subject + '.txt'
     Q = open( filename, 'r' )
@@ -46,7 +63,7 @@ for subject in subjects:
         if answer == '': die( f'question at {filename}:{line_num} is not followed by a non-blank answer on the next line: {question}' )
         line_num += 1
 
-        if match( question, f'.*{string}' ) or match( answer, f'.*{string}' ):
+        if (search_question and match( question, f'.*{string}' )) or (search_answer and match( answer, f'.*{string}' )):
             print()
             print( f'{filename}:{ques_line_num}:    {question}' )
             print( f'{filename}:{line_num}:    {answer}' )
