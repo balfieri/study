1e416e127c6bd9f3d7fab996d825d058e5abc030
Bob Alfieri <bob_alfieri@yahoo.com>
2025-12-22 09:30:47 -0500

    tweaks

--- memdeck.py
+++ memdeck.py
@@ -22,12 +22,22 @@ with open( 'mydeck.json', 'r', encoding='utf-8' ) as f:
 deck_len = len( deck )
 if deck_len != 52: die( f'deck does not have 52 cards, got {deck_len}: {deck}' )
 
-# forward direction
+# forward order
 #
 print( '\nTell me the cards in forward order:\n' )
 i = 0
-for c in deck:
+while i < 52:
     ans = input( f'{i}: ' )
     ans = ans.rstrip()
     if ans != deck[i]: print( f'*{deck[i]}' )
     i += 1
+
+# reverse order
+#
+print( '\nTell me the cards in reverse order:\n' )
+i = 51
+while i >= 0:
+    ans = input( f'{i}: ' )
+    ans = ans.rstrip()
+    if ans != deck[i]: print( f'*{deck[i]}' )
+    i -= 1
