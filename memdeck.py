de213000e9c8dc40e7c890d886e58c593c93d79f
Bob Alfieri <bob_alfieri@yahoo.com>
2025-12-23 10:09:32 -0500

    tweaks

--- memdeck.py
+++ memdeck.py
@@ -12,14 +12,34 @@ def die( msg, prefix='ERROR: ' ):
     else:
         sys.exit( 1 )
 
+def deck_print( deck, i, j ):
+    tot = 0
+    c = 0
+    while i <= j:
+        if c == 0: 
+            print( f'{i:2d}: ', end='' )
+        else:
+            print( '  ', end='' )
+        card = deck[i]
+        print( f'{card:3s}', end='' )
+        tot += 1
+        c += 1
+        if c == 4 or (tot % 13) == 0:
+            print()
+            if (tot % 13) == 0: print()
+            c = 0
+        i += 1
+
 #-----------------------------------------------------------------------
 # process command line args
 #-----------------------------------------------------------------------
 if len( sys.argv ) < 1: die( 'usage: memdeck.py [options]', '' )
 json_filename = 'mydeck.json'
 do_print_deck = False
-do_forward = True
-do_reverse = True
+fwd_i_begin = 0
+fwd_i_last  = 52
+rev_i_begin = 51
+rev_i_last  = 0
 i = 1
 while i < len( sys.argv ):
     arg = sys.argv[i]
@@ -28,10 +48,22 @@ while i < len( sys.argv ):
         json_filename = sys.argv[i]
     elif arg == '-pd':
         do_print_deck = int(sys.argv[i])
-    elif arg == '-f':
-        do_forward = int(sys.argv[i])
-    elif arg == '-r':
-        do_reverse = int(sys.argv[i])
+    elif arg == '-fb':
+        fwd_i_begin = int(sys.argv[i])
+    elif arg == '-fl':
+        fwd_i_last = int(sys.argv[i])
+    elif arg == '-fq': 
+        q = int(sys.argv[i])
+        fwd_i_begin = 13*q + 0
+        fwd_i_last  = 13*q + 12
+    elif arg == '-rb':
+        rev_i_begin = int(sys.argv[i])
+    elif arg == '-rl':
+        rev_i_last = int(sys.argv[i])
+    elif arg == '-rq': 
+        q = int(sys.argv[i])
+        rev_i_begin = 13*q + 12
+        rev_i_last  = 13*q + 0
     else:
         i -= 1
         break
@@ -44,29 +76,31 @@ with open( json_filename, 'r', encoding='utf-8' ) as f:
     data = json.load( f )
     deck = data['deck']
 
-if do_print_deck: print( deck )
+if do_print_deck: deck_print( deck, 0, 51 )
 deck_len = len( deck )
 if deck_len != 52: die( f'deck does not have 52 cards, got {deck_len}: {deck}' )
 
 #-----------------------------------------------------------------------
 # forward
 #-----------------------------------------------------------------------
-if do_forward:
+if fwd_i_begin <= fwd_i_last:
     print( '\nTell me the cards in forward order:\n' )
-    i = 0
-    while i < 52:
+    i = fwd_i_begin
+    while i <= fwd_i_last:
         ans = input( f'{i}: ' )
         ans = ans.rstrip()
         if ans != deck[i]: print( f'*{deck[i]}' )
         i += 1
 
+deck_print( deck, fwd_i_begin, fwd_i_last )
+
 #-----------------------------------------------------------------------
 # reverse
 #-----------------------------------------------------------------------
-if do_reverse:
+if rev_i_last >= rev_i_begin:
     print( '\nTell me the cards in reverse order:\n' )
-    i = 51
-    while i >= 0:
+    i = rev_i_last
+    while i >= rev_i_begin:
         ans = input( f'{i}: ' )
         ans = ans.rstrip()
         if ans != deck[i]: print( f'*{deck[i]}' )
